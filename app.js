9a3cd67f682fe70600a1e1e7becc8adcbf22dbdc
Zachary Shirmohammadli <zachshirow@gmail.com>
2022-02-04 18:20:58 +0330

    renamed the excel files using the data in their cells

--- app.js
+++ app.js
@@ -1,22 +1,65 @@
-const XLSX = require('xlsx');
+const fs = require("fs");
+const XLSX = require("xlsx");
 
+//to do
+// 1) get the list of files in the directory
+// 2) in a map, rename the files to the format Degree - UserID - RequestID
+// 3) create another array with the new file names and append each file name that is created to this array
+// 4) create a new object or excel workbook where you can add the needed data later on
+// 5) loop over the new file names and get the needed fields from them. add the fields based on their degree to three different sheets
 
+const excelFileNames = fs.readdirSync("./excel-data");
+const excelFileNamesRevised = [];
 
+// rename the files and push to the array of filenames
+excelFileNames.map((f) => {
+	// I first need to rename the file as follows: Degree - UserID - RequestID
+	// read the file
+	const workbook = XLSX.readFile(`./excel-data/${f}`);
 
-var workbook = XLSX.readFile('excel-data/164373266889.xlsx');
+	// the available sheets
+	const personalInfo = workbook.SheetNames[1];
+	const step2 = workbook.SheetNames[2];
+	const step3 = workbook.SheetNames[3];
+	const step4 = workbook.SheetNames[4];
 
-var second_sheet_name = workbook.SheetNames[1];
-var address_of_cell = 'A2';
+	const personalInfoSheet = workbook.Sheets[personalInfo];
+	const step2Sheet = workbook.Sheets[step2];
+	const step3Sheet = workbook.Sheets[step3];
+	const step4Sheet = workbook.Sheets[step4];
 
+	// required cells for doing the rename
+	const degree = step2Sheet["D2"] ? step2Sheet["D2"].v : undefined;
+	const userID = step2Sheet["B2"] ? step2Sheet["B2"].v : undefined;
+	const requestID = personalInfoSheet["D2"]
+		? personalInfoSheet["D2"].v
+		: undefined;
 
-/* Get worksheet */
-var worksheet = workbook.Sheets[second_sheet_name];
+	const newFileName = `./excel-data/${degree} - ${userID} - ${requestID}.xlsx`;
 
-/* Find desired cell */
-var desired_cell = worksheet[address_of_cell];
+	fs.renameSync(`./excel-data/${f}`, newFileName);
 
-/* Get the value */
-var desired_value = (desired_cell ? desired_cell.v : undefined);
+	excelFileNamesRevised.push(newFileName);
+});
 
-console.log(desired_value)
+excelFileNamesRevised.map((f) => {
+	const workbook = XLSX.readFile(f);
+	console.log(f);
+	// the available sheets
+	const personalInfo = workbook.SheetNames[1];
+	const step2 = workbook.SheetNames[2];
+	const step3 = workbook.SheetNames[3];
+	const step4 = workbook.SheetNames[4];
 
+	const personalInfoSheet = workbook.Sheets[personalInfo];
+	const step2Sheet = workbook.Sheets[step2];
+	const step3Sheet = workbook.Sheets[step3];
+	const step4Sheet = workbook.Sheets[step4];
+
+	// required cells for doing the rename
+	const degree = step2Sheet["D2"] ? step2Sheet["D2"].v : undefined;
+	const userID = step2Sheet["B2"] ? step2Sheet["B2"].v : undefined;
+	const requestID = personalInfoSheet["D2"]
+		? personalInfoSheet["D2"].v
+		: undefined;
+});
